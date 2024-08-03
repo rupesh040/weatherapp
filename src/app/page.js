6c3ad9cc3476f9091ef22b0cbc5ad1980845b292
rupesh040 <112106535+rupesh040@users.noreply.github.com>
2024-08-03 12:25:02 +0530

    something improve

--- src/app/page.js
+++ src/app/page.js
@@ -9,12 +9,15 @@ export default function Home() {
     const [weatherData ,setWeatherData] = useState({});
     const search = async (city)=> {
 try {
+    if(inputRef===""){
+        alert("enter city name");
+    }
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${api}`
     const response = await fetch(url);
     const data = await response.json();
     console.log(data);
     setWeatherData({
-        temperature:data.main.temp,
+        temperature:Math.trunc(data.main.temp),
         humidity:data.main.humidity,
         windSpeed:data.wind.speed,
         location:data.name
@@ -29,8 +32,8 @@ try {
   return (
    <>
    <div class="container w-[100vw] h-[100vh] overflow-hidden flex justify-center items-center ">
-    <div class="weather w-80 h-96 bg-white rounded-2xl overflow-hidden flex flex-col justify-between text-[#201f1f] z-10">
-        <div className="flex justify-center px-4 py-3 gap-3 bg-[#201f1f] "><input type="text" className="text-[18px] px-2 outline-none bg-transparent text-white" ref={inputRef} placeholder="Enter City Name" autoFocus/> <i class="ri-search-line text-[20px] cursor-pointer text-zinc-50" onClick={()=> search (inputRef.current.value)}></i></div>
+    <div class="weather w-80 h-96  rounded-2xl overflow-hidden flex flex-col justify-between text-[#201f1f] z-10">
+        <div className="flex justify-center px-4 py-3 gap-3  input"><input type="text" className="text-[18px] px-2 outline-none bg-transparent text-white" ref={inputRef} placeholder="Enter City Name" autoFocus/> <i class="ri-search-line text-[20px] cursor-pointer text-zinc-50" onClick={()=> search (inputRef.current.value)}></i></div>
         <div className="flex  flex-col items-center">
         <h1 className=" text-[50px] font-bold">{weatherData.temperature}°C</h1>
         <h2 className=" text-[30px] font-semibold" >{weatherData.location}</h2>
